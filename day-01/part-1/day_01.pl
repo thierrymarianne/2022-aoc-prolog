5bd155ab1be5baa0ef907a38145315ea16d24310
Thierry Marianne <thierry@marianne.io>
2023-02-07 22:19:47 +0100

    Solving day 1 - part 1

---
+++ day-01/part-1/day_01.pl
@@ -0,0 +1,63 @@
+:- module(day_01,[read_file/1]).
+
+:- use_module(engine(basic_props)).
+:- use_module(engine(messages_basic)).
+:- use_module(library(assertions/native_props)).
+:- use_module(library(format)).
+:- use_module(library(lists)).
+:- use_module(library(numlists)).
+:- use_module(library(stream_utils)).
+:- use_module(library(terms_io)).
+:- use_module(library(terms)).
+:- use_module(library(strings)).
+:- use_module(library(when)).
+
+%:- use_package(debug).
+%:- use_package(trace).
+
+not_eos(Stream) :-
+    peek_byte(Stream, Byte),
+    Byte =\= -1.
+
+read_until_next_whitespace_shows_up(Stream, InitialValue, PreviousSum, Sum) :-
+    peek_code(Stream, Code),
+    Code =:= 10,
+    get_line(Stream, Line),
+    line_count(Stream, _ElfOrdinal),
+    length(Line, _Length),
+    _Length =:= 0,
+    if(
+        Sum > PreviousSum,
+        read_until_next_whitespace_shows_up(Stream, InitialValue, InitialValue, Sum),
+        read_until_next_whitespace_shows_up(Stream, InitialValue, InitialValue, PreviousSum)
+    ).
+
+read_until_next_whitespace_shows_up(Stream, InitialValue, PreviousSum, MostCalories) :-
+    if(
+        not_eos(Stream),
+        (
+            get_line(Stream, Line),
+            length(Line, _Length),
+            number_codes(_Calories, Line),
+            _Length > 0,
+            format('Next food item contains ~s calories.~n', [Line]),
+            _NextCaloriesTotal is +(PreviousSum, _Calories),
+            format('Next sum is ~d.~n~n', [_NextCaloriesTotal]),
+            read_until_next_whitespace_shows_up(Stream, InitialValue, _NextCaloriesTotal, MostCalories)
+        ),
+        format('Most calories are ~d.~n~n', [MostCalories])
+    ).
+
+read_file(File) :-
+    gnd(File),
+    file_exists(File),
+    format("~n~n[ Opening file ~q ]~n~n", ['./input.txt']),
+    open(File, read, _Stream),
+    read_until_next_whitespace_shows_up(_Stream, 0, 0, 0).
+
+%
+% Where to find usage examples?
+% What are the differences between a module and a package ("hiord" package)?
+% Is [repeat/2 documentation](https://ciao-lang.org/ciao/build/doc/ciao.html/ciaosearch.html#repeat/2) correct?
+% Where can I find some hosted version of [PiLLoW's Web programming libraries](http://cliplab.org/logalg/slides/C_pillow/C_pillow.pdf)?
+%
