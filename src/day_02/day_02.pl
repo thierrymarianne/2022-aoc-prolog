ac20eb7ab6c11fd82aa0efd392da2c272243c07a
Thierry Marianne <thierry@marianne.io>
2023-02-09 21:08:51 +0100

    Solving day 2 - part 1 and 2

--- src/day_02/day_02.pl
+++ src/day_02/day_02.pl
@@ -1,116 +1,210 @@
-% consult('./day_02').
-% read_file('./input.txt').
-:- module(day_02, [day_02/2]).
+% swipl -s ./day_02.pl
+:- module(day_02, [day_02/1]).
 
-:- use_module(engine(basic_props)).
-:- use_module(engine(messages_basic)).
-:- use_module(library(assertions/native_props)).
-:- use_module(library(format)).
-:- use_module(library(lists)).
-:- use_module(library(numlists)).
-:- use_module(library(stream_utils)).
-:- use_module(library(terms_io)).
-:- use_module(library(terms)).
-:- use_module(library(strings)).
-:- use_module(library(when)).
-:- use_module(library(sort)).
-:- use_module(library(regexp/regexp_code)).
-
-%:- use_package(debug).
-:- use_package(trace).
+:- use_module(library(readutil)).
 
 not_eos(Stream) :-
-    peek_byte(Stream, Byte),
-    Byte =\= -1.
+    \+at_end_of_stream(Stream).
+    
+print_choice_rock(Choice, Translation) :-
+    (
+        Choice = "A";
+        Choice = "X"
+    ),
+    Translation = "Rock".
 
-%read_until_next_whitespace_shows_up(Stream, InitialValue, Sums, PreviousSum, Sum, _Top3) :-
-%    peek_code(Stream, Code),
-%    Code =:= 10,
-%    get_line(Stream, Line),
-%    length(Line, _Length),
-%    _Length =:= 0,
-%
-%    if(
-%        Sum > PreviousSum,
-%        (
-%            _NextSum is Sum,
-%            _NextMax is PreviousSum
-%        ),
-%        (
-%            _NextSum is PreviousSum,
-%            _NextMax is Sum
-%        )
-%    ),
-%
-%    append(Sums, [_NextMax], L),
-%    read_until_next_whitespace_shows_up(Stream, InitialValue, L, InitialValue, _NextSum, _Top3).
+print_choice_paper(Choice, Translation) :-
+    (
+        Choice = "B";
+        Choice = "Y"
+    ),
+    Translation = "Paper".
 
-read_until_next_whitespace_shows_up(
-    Stream,
-    InitialValue,
-    Sums,
-    PreviousSum,
-    MostCalories,
-    Top3
-) :-
-    if(
-        not_eos(Stream),
+print_choice_scissors(Choice, Translation) :-
+    (
+        Choice = "C";
+        Choice = "Z"
+    ),
+    Translation = "Scissors".
+
+translate_choice(Choice, Translation) :-
+    print_choice_rock(Choice, Translation);
+    print_choice_paper(Choice, Translation);
+    print_choice_scissors(Choice, Translation).
+
+% Rock
+how_many_points_for_rock(Choice, Points) :-
+    (
+        Choice = "A";
+        Choice = "X";
+        Choice = "Rock"
+    ),
+    Points is 1.
+
+% Paper
+how_many_points_for_paper(Choice, Points) :-
+    (
+        Choice = "B";
+        Choice = "Y";
+        Choice = "Paper"
+    ),
+    Points is 2.
+
+% Scissors
+how_many_points_for_scissors(Choice, Points) :-
+    (
+        Choice = "C";
+        Choice = "Z";
+        Choice = "Scissors"
+    ),
+    Points is 3.
+
+how_many_points(Choice, Points) :-
+    how_many_points_for_scissors(Choice, Points);
+    how_many_points_for_paper(Choice, Points);
+    how_many_points_for_rock(Choice, Points).
+
+what_strategy_for_scissors(OpponentChoice, Tip, StrategicChoice) :-
+    OpponentChoice = "Scissors",
+    (
         (
-            get_line(Stream, Line),
-            length(Line, _Length),
-            string(Line),
-            line(Line),
-            last(Line, _LastItem),
-            trace,
-            match_posix_matches("\(b|a\)\(a|b\)", "ba", Matches),
-            format("Line is ~q~n", [Matches]),
-            match_posix("66**", Line, Matches, Rest),
-            format("Matches are ~q~n", [Rest, Matches])
-%            read_until_next_whitespace_shows_up(
-%                Stream,
-%                InitialValue,
-%                Sums,
-%                +(PreviousSum, _Calories),
-%                MostCalories,
-%                Top3
-%            )
-        ),
+            Tip = "X",
+            StrategicChoice = "Paper"
+        );
         (
-%            _MostCalories is MostCalories,
-%            append(Sums, [MostCalories], _Sums),
-%            sort(_Sums, SortedSums),
-%            reverse(SortedSums, RevSortedSums),
-%
-%            nth(1, RevSortedSums, _First),
-%            nth(2, RevSortedSums, _Second),
-%            nth(3, RevSortedSums, _Third),
-%
-%            sum_list([_First, _Second, _Third], Sum),
-%            format('The maximum amount of calories carried by the 3 topmost elves are ~d.~n~n', [Sum]),
-
-            close(Stream),
-            format('Closed stream.~n~n', [])
+            Tip = "Y",
+            StrategicChoice = "Scissors"
+        );
+        (
+            Tip = "Z",
+            StrategicChoice = "Rock"
         )
     ).
 
-day_02(File, [A, B, C]) :-
-    working_directory(_CurrentDir, '/home/sh4l/labodev/repositories/studying/advent-of-code/src'),
+what_strategy_for_rock(OpponentChoice, Tip, StrategicChoice) :-
+    OpponentChoice = "Rock",
+    (
+        (
+            Tip = "X",
+            StrategicChoice = "Scissors"
+        );
+        (
+            Tip = "Y",
+            StrategicChoice = "Rock"
+        );
+        (
+            Tip = "Z",
+            StrategicChoice = "Paper"
+        )
+    ).
+
+what_strategy_for_paper(OpponentChoice, Tip, StrategicChoice) :-
+    OpponentChoice = "Paper",
+    (
+        (
+            Tip = "X",
+            StrategicChoice = "Rock"
+        );
+        (
+            Tip = "Y",
+            StrategicChoice = "Paper"
+        );
+        (
+            Tip = "Z",
+            StrategicChoice = "Scissors"
+        )
+    ).
+
+what_strategy_for(OpponentChoice, Tip, StrategicChoice) :-
+    what_strategy_for_paper(OpponentChoice, Tip, StrategicChoice);
+    what_strategy_for_rock(OpponentChoice, Tip, StrategicChoice);
+    what_strategy_for_scissors(OpponentChoice, Tip, StrategicChoice).
+
+how_many_points_for_winner(PlayerAChoice, PlayerBChoice, Points) :-
+    (
+        PlayerAChoice = "Scissors",
+        PlayerBChoice = "Paper"
+    ;
+        PlayerAChoice = "Rock",
+        PlayerBChoice = "Scissors"
+    ;
+        PlayerAChoice = "Paper",
+        PlayerBChoice = "Rock"
+    ),
+    Points is 6.
+
+how_many_points_for_both(PlayerAChoice, PlayerBChoice, Points) :-
+    (
+        PlayerAChoice = "Scissors",
+        PlayerBChoice = "Scissors"
+    ;
+        PlayerAChoice = "Rock",
+        PlayerBChoice = "Rock"
+    ;
+        PlayerAChoice = "Paper",
+        PlayerBChoice = "Paper"
+    ),
+    Points is 3.
+
+how_many_points_for_loser(PlayerAChoice, PlayerBChoice, Points) :-
+    (
+        PlayerAChoice = "Paper",
+        PlayerBChoice = "Scissors"
+    ;
+        PlayerAChoice = "Scissors",
+        PlayerBChoice = "Rock"
+    ;
+        PlayerAChoice = "Rock",
+        PlayerBChoice = "Paper"
+    ),
+    Points is 0.
+
+how_many_points_given_match_outcome(PlayerAChoice, PlayerBChoice, Points) :-
+    how_many_points_for_both(PlayerAChoice, PlayerBChoice, Points);
+    how_many_points_for_loser(PlayerAChoice, PlayerBChoice, Points);
+    how_many_points_for_winner(PlayerAChoice, PlayerBChoice, Points).
+
+read_until_next_whitespace_shows_up(Stream, PreviousPoints, TotalPoints) :-
+    (
+        not_eos(Stream) ->
+            read_line_to_codes(Stream, Line),
+            re_matchsub("(.+) (.+)", Line, Matches),
+            get_dict(1, Matches, OpponentChoice),
+            how_many_points(OpponentChoice, OpponentPoints),
+
+            translate_choice(OpponentChoice, OpponentChoiceTranslation),
+
+            get_dict(2, Matches, Tip),
+            what_strategy_for(OpponentChoiceTranslation, Tip, StrategicChoice),
+            format("What strategic choice to make against ~s: ~s (~s)~n", [OpponentChoiceTranslation, StrategicChoice, Tip]),
+            how_many_points(StrategicChoice, OurPoints),
+
+            how_many_points_given_match_outcome(OpponentChoiceTranslation, StrategicChoice, OpponentsPointsForMatchOutcome),
+            how_many_points_given_match_outcome(StrategicChoice, OpponentChoiceTranslation, OurPointsForMatchOutcome),
+
+            format("Opponent choice: ~s (~d pts)~n", [OpponentChoiceTranslation, OpponentPoints]),
+            format("Our choice ~s (~d pts)~n", [StrategicChoice, OurPoints]),
+            format("Match outcome points (opponent): ~d pts~n", [OpponentsPointsForMatchOutcome]),
+            format("Match outcome points (us): ~d pts~n", [OurPointsForMatchOutcome]),
+            format("Total points so far for us: ~d pts~n~n", [PreviousPoints + OurPoints + OurPointsForMatchOutcome]),
+            read_until_next_whitespace_shows_up(Stream, PreviousPoints + OurPoints + OurPointsForMatchOutcome, TotalPoints)
+        ;
+            TotalPoints is PreviousPoints,
+            close(Stream),
+            format('Closed stream.~n~n', []),
+            halt
+    ).
+
+day_02(TotalPoints) :-
     working_directory(_d, _d),
     format("Current dir is ~q~n~n", _d),
 
-    atom_concat([_d, '/day_02/', File], AbsoluteFilePath),
+    atomic_list_concat([_d, 'input.txt'], AbsoluteFilePath),
     
-    gnd(AbsoluteFilePath),
-    file_exists(AbsoluteFilePath),
+    ground(AbsoluteFilePath),
+    exists_file(AbsoluteFilePath),
     format("~n~n[ Opening file ~q ]~n~n", [AbsoluteFilePath]),
 
-    open(AbsoluteFilePath, read, _Stream),
+    open(AbsoluteFilePath, read, Stream, [buffer(line), close_on_abort(true)]),
 
-    read_until_next_whitespace_shows_up(
-        _Stream,
-        0,
-        [],
-        0,
-        0,
-        [A, B, C|_]
-    ).
+    read_until_next_whitespace_shows_up(Stream, 0, TotalPoints).
